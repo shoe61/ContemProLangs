dd25399c6e9e8ff38edb6089dec1158562b620d9
Scott Schumacher <scottmachershoe@yahoo.com>
2016-09-14 21:27:59 -0500

    added binary-decimal converter windows app

---
+++ progs/Shoe_Assignment_3/Shoe_Assignment_3/BinaryDecimalConverter.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Shoe_Assignment_3
+{
+    partial class BinaryDecimalConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BinaryDecimalConverter));
+            this.inputGroupBox = new System.Windows.Forms.GroupBox();
+            this.binaryIntegerTextBox = new System.Windows.Forms.TextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.OutputGroupBox = new System.Windows.Forms.GroupBox();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.inputGroupBox.SuspendLayout();
+            this.OutputGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // inputGroupBox
+            // 
+            this.inputGroupBox.BackColor = System.Drawing.Color.Lime;
+            this.inputGroupBox.Controls.Add(this.convertButton);
+            this.inputGroupBox.Controls.Add(this.binaryIntegerTextBox);
+            this.inputGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputGroupBox.Location = new System.Drawing.Point(31, 12);
+            this.inputGroupBox.Name = "inputGroupBox";
+            this.inputGroupBox.Size = new System.Drawing.Size(289, 152);
+            this.inputGroupBox.TabIndex = 0;
+            this.inputGroupBox.TabStop = false;
+            this.inputGroupBox.Text = "Please enter a binary integer:";
+            // 
+            // binaryIntegerTextBox
+            // 
+            this.binaryIntegerTextBox.Location = new System.Drawing.Point(77, 39);
+            this.binaryIntegerTextBox.Name = "binaryIntegerTextBox";
+            this.binaryIntegerTextBox.Size = new System.Drawing.Size(150, 26);
+            this.binaryIntegerTextBox.TabIndex = 0;
+            this.binaryIntegerTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // convertButton
+            // 
+            this.convertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convertButton.Location = new System.Drawing.Point(77, 86);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(150, 50);
+            this.convertButton.TabIndex = 1;
+            this.convertButton.Text = "Click to convert to decimal";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // OutputGroupBox
+            // 
+            this.OutputGroupBox.BackColor = System.Drawing.Color.Lime;
+            this.OutputGroupBox.Controls.Add(this.resultLabel);
+            this.OutputGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OutputGroupBox.Location = new System.Drawing.Point(31, 205);
+            this.OutputGroupBox.Name = "OutputGroupBox";
+            this.OutputGroupBox.Size = new System.Drawing.Size(289, 89);
+            this.OutputGroupBox.TabIndex = 1;
+            this.OutputGroupBox.TabStop = false;
+            this.OutputGroupBox.Text = "The decimal equivalent is:";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(77, 36);
+            this.resultLabel.MaximumSize = new System.Drawing.Size(150, 29);
+            this.resultLabel.MinimumSize = new System.Drawing.Size(150, 29);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(150, 29);
+            this.resultLabel.TabIndex = 0;
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BinaryDecimalConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(348, 326);
+            this.Controls.Add(this.OutputGroupBox);
+            this.Controls.Add(this.inputGroupBox);
+            this.Name = "BinaryDecimalConverter";
+            this.Text = "Shoe\'s Binary to Decimal Converter";
+            this.inputGroupBox.ResumeLayout(false);
+            this.inputGroupBox.PerformLayout();
+            this.OutputGroupBox.ResumeLayout(false);
+            this.OutputGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox inputGroupBox;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.TextBox binaryIntegerTextBox;
+        private System.Windows.Forms.GroupBox OutputGroupBox;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ progs/Shoe_Assignment_3/Shoe_Assignment_3/BinaryDecimalConverter.cs
@@ -0,0 +1,51 @@
+﻿//Scott Schumacher
+//9-15-2016
+//FileName: BinaryDecimalConverter.cs
+//This program invites the user to enter a binary integer in a text box and
+//to then click a button; this event triggers the button's event handler,
+//which converts the binary number to its decimal equivalent and displays
+//the result in a label.
+
+
+using System;
+using System.Windows.Forms;
+
+namespace Shoe_Assignment_3
+{
+    public partial class BinaryDecimalConverter : Form
+    {
+        public BinaryDecimalConverter()
+        {
+            InitializeComponent();
+        }
+        //event handler for button:
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            //read input string from text box; parse as integer
+            int binNum = int.Parse(binaryIntegerTextBox.Text);
+ 
+            //count the digits to control loop below
+            int numDigits = binaryIntegerTextBox.Text.Length;
+
+            //declare variable for output
+            int decNum = 0;
+
+            //with each iteration of the loop, strip off trailing digit by 
+            //performing modulus followed by integer division by ten
+            for(int i = 0; i < numDigits; i++)
+            {
+                int opr = binNum % 10;
+                binNum /= 10;
+                
+                //This loop is necessary to ensure that a zero in the one's place
+                //(positionally, the 2 to the power 0 place) is not computed as 1.
+                if(opr > 0)
+                    {
+                        decNum += (int)(Math.Pow((double)opr * 2, (double)i));
+                    }
+            }
+            //output directed to label
+            resultLabel.Text = decNum.ToString();
+        }
+    }
+}
