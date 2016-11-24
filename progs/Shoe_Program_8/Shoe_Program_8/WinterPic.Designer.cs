9a69c74c492bc5018abb6232454b1646e0d9d3a0
scott schumacher <scottmachershoe@yahoo.com>
2016-11-24 15:30:21 -0600

    adding

---
+++ progs/Shoe_Program_8/Shoe_Program_8/WinterPic.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Shoe_Program_8
+{
+    partial class WinterPic
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imagePanel = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // imagePanel
+            // 
+            this.imagePanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imagePanel.Location = new System.Drawing.Point(12, 12);
+            this.imagePanel.Name = "imagePanel";
+            this.imagePanel.Size = new System.Drawing.Size(1244, 496);
+            this.imagePanel.TabIndex = 0;
+            // 
+            // WinterPic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1268, 568);
+            this.Controls.Add(this.imagePanel);
+            this.Name = "WinterPic";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "A Winter Scene";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel imagePanel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ progs/Shoe_Program_8/Shoe_Program_8/WinterPic.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shoe_Program_8
+{
+    public partial class WinterPic : Form
+    {
+        public WinterPic()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            using (Graphics grafix = this.imagePanel.CreateGraphics())
+            {
+                SolidBrush shoeBrush = new SolidBrush(Color.Red);
+                Pen shoePen = new Pen(Color.Black);
+                Font shoeFont = new Font("Ariel", 32);
+
+                grafix.DrawString("First, the required elements.", shoeFont, shoeBrush, 25, 25);
+                grafix.DrawString("Rectangles, drawn and filled:", shoeFont, shoeBrush, 25, 100);
+                grafix.DrawString("Ellipses, drawn and filled: ", shoeFont, shoeBrush, 25, 200);
+                grafix.DrawString("Now I have a problem, because I'm no artist, and Texas has ", shoeFont, shoeBrush, 25, 300);
+                grafix.DrawString("no winter to speak of... ", shoeFont, shoeBrush, 25, 400);
+                
+                grafix.DrawRectangle(shoePen, 600, 100, 200, 50);
+                grafix.FillRectangle(shoeBrush, 900, 100, 200, 50);
+
+                grafix.DrawEllipse(shoePen, 600, 200, 200, 50);
+                grafix.FillEllipse(shoeBrush, 900, 200, 200, 50);
+
+                
+
+                Point a = new Point(0,400);
+                Point b = new Point(1000, 800);
+                Point c = new Point(0, 800);
+                Point d = new Point(1000, 400);
+
+                Point[] hillPoints = {a, b, c, d};
+
+                //grafix.DrawPolygon(shoePen, hillPoints);
+                //grafix.DrawLines(shoePen, hillPoints);
+            }
+        }
+
+
+
+       
+    }
+}
