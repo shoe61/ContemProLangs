b13182fc7a77774705f6c6f77a1f191f8dabff9d
scott schumacher <scottmachershoe@yahoo.com>
2016-12-29 08:11:06 -0600

    added hidden word game

---
+++ Java/sp1/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+* Scott Schumacher
+* Hello World
+* This program is a simple demonstration of a Java text output to console.
+* December 18, 2016
+*/
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		String stringy = "Shoe says 'Hello, World!'";
+		System.out.println(stringy);
+	}
+	
+}
